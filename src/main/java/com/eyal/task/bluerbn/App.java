12ae1cefa96800e4aa11b8b7f0903cf5bf198b91
unknown <eyalmar100@gmail.com>
2020-09-26 11:26:33 +0300

    first commit

---
+++ src/main/java/com/eyal/task/bluerbn/App.java
@@ -0,0 +1,13 @@
+package com.eyal.task.bluerbn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
